7535d226f38b1c15bc1d6683e4cf7dc39c2f1e64
fh <fh-github@fholzhauer.de>
2014-03-04 10:14:48 +0100

    Using phpenv for modules in php.ini

--- travis/modulecache.sh
+++ travis/modulecache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MODULE_CACHE_DIR=${TRAVIS_BUILD_DIR}/travis/module-cache/`php-config --vernum`
+INI_DIR=${TRAVIS_BUILD_DIR}/travis/ini/
 PHP_TARGET_DIR=`php-config --extension-dir`
 
 if [ -d ${MODULE_CACHE_DIR} ]
@@ -7,6 +8,7 @@ then
   cp ${MODULE_CACHE_DIR}/* ${PHP_TARGET_DIR}
 fi
 
+mkdir -p ${INI_DIR}
 mkdir -p ${MODULE_CACHE_DIR}
 
 for module in $MODULES
@@ -18,9 +20,9 @@ do
     echo "$FILENAME not found in extension dir, compiling"
     printf "yes\n" | pecl install ${PACKAGE}
   else
-    # http://blog.travis-ci.com/2013-03-08-preinstalled-php-extensions/
     echo "Adding $FILENAME to php config"
-    echo "extension = $FILENAME" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    echo "extension = $FILENAME" > ${INI_DIR}/${FILENAME}.ini
+    phpenv config-add ${INI_DIR}/${FILENAME}.ini
   fi
   cp ${PHP_TARGET_DIR}/${FILENAME} ${MODULE_CACHE_DIR}
 done
